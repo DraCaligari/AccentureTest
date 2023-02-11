6fc4b799c9052ea4f5aeb274bc061f6ccadf5606
DraCaligari <tatianagarcia0695@hotmail.com>
2023-02-11 15:41:51 -0600

    Posts
    Posts of the page to responsive page

---
+++ ReactAccenture/src/components/Posts.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import "../Styles/posts.css"
+
+export default function Posts() {
+
+    {/*post page body of the page*/}
+    
+  return (
+        <div className="container text-center">
+            <div className="row">
+                <div className="col-6">
+                    <img className='size' src="https://images.unsplash.com/photo-1673238690947-5ef76901c39d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE0OTI3Nw&ixlib=rb-4.0.3&q=80&w=1080" alt="A beach in th night" />
+                    <div className='text-preview'>
+                        <h3>Beach in the night</h3>
+                        <p>Comments</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget leo a enim volutpat rutrum. Duis suscipit consectetur sem nec fermentum.</p>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <img className='size' src="https://images.unsplash.com/photo-1673361692979-42ec8162b6f7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE0OTMyMg&ixlib=rb-4.0.3&q=80&w=1080" alt="A beach in th night" />
+                    <div className='text-preview'>
+                        <h3>Lorem ipsum dolor sit amet</h3>
+                        <p>Comments</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget leo a enim volutpat rutrum. Duis suscipit consectetur sem nec fermentum.</p>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <img className='size' src="https://images.unsplash.com/photo-1675685693854-e995811168bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE0OTM1OQ&ixlib=rb-4.0.3&q=80&w=1080" alt="A beach in th night" />
+                    <div className='text-preview'>
+                        <h3>Lorem ipsum dolor sit amet</h3>
+                        <p>Comments</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget leo a enim volutpat rutrum. Duis suscipit consectetur sem nec fermentum.</p>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <img className='size' src="https://images.unsplash.com/photo-1674064057052-a943a5538926?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE0OTM4Mw&ixlib=rb-4.0.3&q=80&w=1080" alt="A beach in th night" />
+                    <div className='text-preview'>
+                        <h3>Lorem ipsum dolor sit amet</h3>
+                        <p>Comments</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget leo a enim volutpat rutrum. Duis suscipit consectetur sem nec fermentum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+  )
+}
